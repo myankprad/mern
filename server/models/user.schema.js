0d031ddf127f79647d1cc5124349cd1e0028033a
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-05-04 13:33:41 +0530

    jwt

--- server/models/user.schema.js
+++ server/models/user.schema.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -24,8 +26,42 @@ const userSchema = new mongoose.Schema({
     cpassword:{
         type: String,
         require :true
-    }
+    }, tokens:[
+        {
+            token :{
+                type: String,
+                require :true
+            }
+        }
+    ]
 })
+
+
+
+// password hashing
+
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")){
+        this.password = bcrypt.hash(this.password, 12)
+        this.cpassword = bcrypt.hash(this.cpassword, 12)
+
+    }
+    next()
+})
+
+// generating token 
+const SECRET_KEY = process.env.SECRET_KEY
+userSchema.methods.generateAuthToken = async  function(){
+    try{
+        token = jwt.sign({_id : this._id}, process.env.SECRET_KEY)
+        this.tokens = this.tokens.concat({token : token })
+        await this.save()
+        return token
+    } catch(err){
+console.log(err)
+    }
+}
+
 
 const User = mongoose.model('USER', userSchema)
 
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,41 +1,78 @@
-const express = require("express")
+const express = require("express");
+const bcrypt = require("bcryptjs")
+const router = express.Router();
+const jwt = require("jsonwebtoken")
 
-const router = express.Router()
+require("../db/conn");
+const User = require("../models/user.schema");
 
-require("../db/conn")
-const User = require("../models/user.schema")
+router.get("/", (req, res) => {
+  res.send("Hey");
+});
 
-router.get("/", (req, res)=>{
-    res.send("Hey")
-})
+router.post("/register", async (req, res) => {
+    let token
+  const { name, email, phone, work, password, cpassword } = req.body;
 
-router.post("/register", async (req, res)=>{
-   const {name, email, phone, work, password, cpassword}= req.body;
-
-   if(!name || !email || !phone || !work || !password ||!cpassword){
-    return res.status(422).json({error: "fil all fields"})
-   }
-
-  try{
-     const userExist = await  User.findOne({email: email}) 
-      if(userExist){
-      return res.status(422).json({error: "Email already exist"})
-      }
-  
-      const user = new User({name, email, phone, work, password, cpassword})
-
-      const userRegister = await user.save();
-      if(userRegister){
-      res.status(422).json({error: "user registered succesfully"})
-        
-      } else{
-        res.status(500).json({error: "failed to register"})
-  
-      }
-  } catch(err){
-    console.log(err)
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "fil all fields" });
   }
-})
 
-module.exports = router
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "Email already exist" });
+    } else if (password != password) {
+      return res.status(422).json({ error: "Password are not matching" });
+    } else {
+      const user = new User({ name, email, phone, work, password, cpassword });
+      //   here password hashing
+      await user.save();
+      res.status(201).json({ error: "user registred sucessfully" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
+router.post("./signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(422).json({ error: "please fill the data" });
+    }
+
+    const userLogin = await User.findOne({ email: email });
+    // console.log(userLogin);
+
+
+
+  
+  if(userLogin){
+
+    const isMatch = await bcrypt.compare(password, userLogin.password)
+
+      token = await userLogin.generateAuthToken()
+
+    if (!isMatch) {
+        res.status(400).json({ error: "user error" });
+      } else {
+        res.json({ message: "userSignIn successfully" });
+      } }
+      
+      else {
+        res.status(400).json({ error: "user error" });
+  
+      }
+  
+
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
+
+
+// 
